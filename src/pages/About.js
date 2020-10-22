3ff294af0bc44016820166e804c292d2dff5fb46
mdemartino26 <55245840+mdemartino26@users.noreply.github.com>
2020-10-21 23:56:24 -0300

    style component y about

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,8 @@ import {NavBar} from "../components/NavBar";
 import {TitleH2} from '../components/TitleH2';
 import {DivNosIndividual} from '../components/DivNosIndividual'
 import LoginButton from "../components/LoginButton";
-import LogoutButton from "../components/LogoutButton"
+import LogoutButton from "../components/LogoutButton";
+import {TitleH1} from '../components/TitleH1'; 
 
 const Home = () => 
 {
@@ -25,7 +26,7 @@ const Home = () =>
                    
                         
                     <Logo263 ToSide={true}></Logo263>
-                       
+                    <TitleH1 Color="#10E3A2" FontS="70px" FontF="HackedFont" style={{color: 'white', margin: 0}} >Nosotros</TitleH1>
                     <div className="Section-Subjects" style={{top:7+"em"}}>
                         <DivNosIndividual Subject="Sujeto #0001" Name="Axel Dumas" PictureInsta="../img/instagram_1.jpeg"/>
                         <DivNosIndividual Subject="Sujeto #0002" Name="Marianela De Martino" PictureInsta="../img/instagram_2.jpeg"/>
--- src/pages/Componentes.js
+++ src/pages/Componentes.js
@@ -8,47 +8,57 @@ import { DivNosIndividual } from '../components/DivNosIndividual'
 import LoginButton from "../components/LoginButton";
 import LogoutButton from "../components/LogoutButton"
 import { Rombo } from '../components/Rombo';
-import { TitleH1 } from '../components/TitleH1'
+import { TitleH1 } from '../components/TitleH1';
+import {DivInfoDash} from '../components/DivInfoDash';
 
 const Componentes = () => {
     return (
         <div className="App">
             <Layout>
-                <div style={{ top: 7 + "em" }}>
+                <div style={{ top: "4em" }}>
 
                     <NavBar>
                     </NavBar>
-                    <TitleH1 Color="#35BDE4" FontS="70px" FontF="HackedFont" style={{ color: 'white', margin: 0 }} >Componentes</TitleH1>
 
-                    <TitleH2 style={{ width: "50%" }} FontS="1em" FontF="Forgotten Futurist" Color="white">
+                    
+                    <TitleH1 Color="#35BDE4" FontS="70px" FontF="HackedFont" style={{ color: 'white', margin: 0 }} >Componentes</TitleH1>
+                    <div>
+                    <TitleH2 style={{ width: "50%" }} FontS="1.8em" FontF="Forgotten Futurist" Color="white">
                         Logo
                         </TitleH2>
                     <Logo263 />
-
-
-
-                    <div style={{ top: 7 + "em" }}>
-                        <TitleH2 style={{ width: "50%" }} FontS="1em" FontF="Forgotten Futurist" Color="white">
-                            Rombos
-                        </TitleH2>
-                        <Rombo Color="#35BDE4" ColorAdentro="#000000" Tamanio="35" />
-                        <Rombo Color="#10E3A2" ColorAdentro="#000000" Tamanio="35" />
-                        <Rombo Color="#E31075" ColorAdentro="#000000" Tamanio="35" />
                     </div>
 
-                    <div style={{ top: 7 + "em" }}>
-                        <TitleH2 style={{ width: "50%" }} FontS="1em" FontF="Forgotten Futurist" Color="white">
-                            Boton
+
+                    <div style={{ padding: "4em"}}>
+                        <TitleH2 style={{ width: "50%" }} FontS="1.8em" FontF="Forgotten Futurist" Color="white">
+                            Rombos
                         </TitleH2>
-                        <BotonWD FontSize="30px" Color="#E31075" ColorAdentro="#181E35" Tamanio="55" style="width:100%">
+                        <Rombo Color="#35BDE4" ColorAdentro="#000000" Tamanio="50" />
+                        <Rombo Color="#10E3A2" ColorAdentro="#000000" Tamanio="50" />
+                        <Rombo Color="#E31075" ColorAdentro="#000000" Tamanio="50" />
+                    </div>
+
+                    <div style={{ top: "4em"}}>
+                        <TitleH2 style={{ width: "50%" }} FontS="1.8em" FontF="Forgotten Futurist" Color="white">
+                            Boton
+                        </TitleH2> <BotonWD FontSize="30px" Color="#E31075" ColorAdentro="#181E35" Tamanio="55" style="width:100%">
                             Boton de ejemplo
                             </BotonWD></div>
 
                     <div className="Section-Subjects" style={{ top: 7 + "em" }}>
-                        <TitleH2 style={{ width: "50%" }} FontS="1em" FontF="Forgotten Futurist" Color="white">
-                            Card
+                        <TitleH2 style={{ width: "50%" }} FontS="1.8em" FontF="Forgotten Futurist" Color="white">
+                            Cards
                         </TitleH2>
-                        <DivNosIndividual Subject="Sujeto #Componente" Name="Texto De Muestra" PictureInsta="../img/instagram_1.jpeg" />
+                        <DivNosIndividual style={{ display: "inline-table" }} Subject="Sujeto #Componente" Name="Texto De Muestra" PictureInsta="../img/instagram_1.jpeg" />
+                        <DivInfoDash style={{ display: "inline-table" }}>
+                            <h3>Super Smash Bros Ultimate</h3>
+                            <div className="Box-Texts">
+                                <h4>Género: Lucha 2D</h4>
+                                <h4>Creador: Mashihiro Sakurai</h4>
+                                <h4>Plataforma: Nintendo Switch</h4>
+                            </div>
+                        </DivInfoDash>
                     </div>
 
 
